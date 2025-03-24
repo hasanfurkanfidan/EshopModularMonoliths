390e4206c27d0793de08c65283374ecc7dfc1aa0
hfidan <hfidan@magiclick.com>
2025-03-24 10:32:50 +0300

    uppdates

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,6 +1,18 @@
+using Catalog;
+using Basket;
+using Ordering;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration)
+    ;
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
