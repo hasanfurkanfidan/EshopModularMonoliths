d6f944a8d14b940b20a832110a21e93def522276
hfidan <hfidan@magiclick.com>
2025-08-10 00:41:29 +0300

    DispatchDomainEventsInterceptor

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,8 +1,10 @@
 ﻿
 using Catalog.Data.Seed;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.IdentityModel.Tokens;
 using Shared.Data.Interceptors;
 using Shared.Data.Seed;
+using System.Reflection;
 
 namespace Catalog
 {
@@ -10,13 +12,23 @@ namespace Catalog
     {
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
+
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
             var connectionString = configuration.GetConnectionString("Database");
 
-            services.AddDbContext<CatalogDbContext>(options =>
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<CatalogDbContext>((sp,options) =>
             {
                 options.UseNpgsql(connectionString);
-                options.AddInterceptors(new AuditableEntityInterceptor());
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
             });
+
             services.AddScoped<IDataSeeder, CatalogDataSeeder>();
             return services;
         }
